1faa17c5fdc5018a17ec8d2de1befaa6b9234989
Connor de la Cruz <connor.c.delacruz@gmail.com>
2023-02-17 14:24:45 -0600

    implemented counter class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      val: 0,
+    }
+  }
+
+  handleClick(amountToAdd) {
+    const currentVal = this.state.val
+    this.setState({
+      val: currentVal + amountToAdd,
+    })
+  }
+
+  render() {
+    return (
+      <div className='counter'>
+        <button
+          className='counter-button'
+          onClick={() => this.handleClick(-1)}>
+          -
+        </button>
+        <span className='counter-value'>{this.state.val}</span>
+        <button
+          className='counter-button'
+          onClick={() => this.handleClick(1)}>
+          +
+        </button>
+      </div>
+    )
+  }
+}
+
+// =============================================================================
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Counter/>)
+
