b3c17cbbdcba23dd79f601cc430678c12cc20f3b
Connor de la Cruz <connor.c.delacruz@gmail.com>
2023-03-09 14:33:49 -0600

    use modern react conventions
    also added mui library

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+
+
+function Counter({ value, decrementOnClick, incrementOnClick }) {
+  return (
+    <div className='counter'>
+      <button
+        className='counter-button decrement'
+        onClick={decrementOnClick}>
+        -
+      </button>
+      <span className='counter-value'>{value}</span>
+      <button
+        className='counter-button increment'
+        onClick={incrementOnClick}>
+        +
+      </button>
+    </div>
+  )
+}
+
+
+function CounterList() {
+  const [counters, setCounters] = useState([{ value: 0}])
+
+  function handleCounterButtonClick(i, amountToAdd) {
+    const newCounters = [...counters]
+    newCounters[i].value = newCounters[i].value + amountToAdd
+    setCounters(newCounters)
+  }
+
+  function renderCounter(i) {
+    return (
+      <Counter
+        value={counters[i].value}
+        incrementOnClick={() => handleCounterButtonClick(i, 1)}
+        decrementOnClick={() => handleCounterButtonClick(i, -1)}
+      />
+    )
+  }
+
+  function handleNewCounterButtonClick() {
+    const newCounter = {
+      value: 0,
+    }
+    const newCounters = [...counters, newCounter]
+    setCounters(newCounters)
+  }
+
+  const counterList = counters.map((counter, i) => {
+    return (
+      <li key={i}>
+        {renderCounter(i)}
+      </li>
+    )
+  })
+  return (
+    <div className='counter-list-container'>
+      <ul className='counter-list'>{counterList}</ul>
+      <button
+        className='counter-list-new-counter-button'
+        onClick={() => handleNewCounterButtonClick()}
+      >
+        Create New Counter
+      </button>
+    </div>
+  )
+}
+
+
+export default function AppContainer() {
+    return (
+      <div className="app-container">
+        <h2>Counters</h2>
+        <CounterList />
+      </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,96 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
+import App from "./App";
 
-// TODO: convert to functional component
-class Counter extends React.Component {
-  render() {
-    return (
-      <div className='counter'>
-        <button
-          className='counter-button decrement'
-          onClick={this.props.decrementOnClick}>
-          -
-        </button>
-        <span className='counter-value'>{this.props.value}</span>
-        <button
-          className='counter-button increment'
-          onClick={this.props.incrementOnClick}>
-          +
-        </button>
-      </div>
-    )
-  }
-}
-
-
-class CounterList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      counters: [
-        {value: 0},
-      ],
-    }
-  }
-
-  handleCounterButtonClick(i, amountToAdd) {
-    const counters = [...this.state.counters]
-    const currentValue = counters[i].value
-    counters[i].value = currentValue + amountToAdd
-    this.setState({
-      counters: counters,
-    })
-  }
-
-  renderCounter(i) {
-    return (
-      <Counter
-        value={this.state.counters[i].value}
-        incrementOnClick={() => this.handleCounterButtonClick(i, 1)}
-        decrementOnClick={() => this.handleCounterButtonClick(i, -1)}
-      />
-    )
-  }
-
-  handleNewCounterButtonClick() {
-    const newCounter = {
-      value: 0,
-    }
-    const counters = [...this.state.counters]
-    this.setState({
-      counters: counters.concat([newCounter]),
-    })
-  }
-
-  render() {
-    const counters = this.state.counters
-    // TODO: add remove counter button
-    const counterList = counters.map((counter, i) => {
-      return (
-        <li key={i}>
-          {this.renderCounter(i)}
-        </li>
-      )
-    })
-
-    return (
-      <div className='counter-list-container'>
-        <h2 className='counter-list-header'>Counters</h2>
-        <ul className='counter-list'>{counterList}</ul>
-        <button
-          className='counter-list-new-counter-button'
-          onClick={() => this.handleNewCounterButtonClick()}
-        >
-          Create New Counter
-        </button>
-      </div>
-    )
-  }
-}
-
-// =============================================================================
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<CounterList />)
-
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
